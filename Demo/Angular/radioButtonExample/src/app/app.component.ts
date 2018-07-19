5520e623b186bbfef15ef9d17c17d7768b589bd8
ipsjolly <ipsj20@gmail.com>
2018-07-19 11:25:09 +0530

    Inline comments added

--- Demo/Angular/radioButtonExample/src/app/app.component.ts
+++ Demo/Angular/radioButtonExample/src/app/app.component.ts
@@ -1,34 +1,36 @@
 import { Component } from '@angular/core';
 
-import {Item} from '../app/item';
-import {ITEMS} from '../app/mock-data';
+      import {Item} from '../app/item';
+      import {ITEMS} from '../app/mock-data';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-  radioSel:any;
-  radioSelected:string;
-  radioSelectedString:string;
-  itemsList: Item[] = ITEMS;
+      @Component({
+        selector: 'app-root',
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.css']
+      })
+      export class AppComponent {
+        title = 'app';
+        radioSel:any;
+        radioSelected:string;
+        radioSelectedString:string;
+        itemsList: Item[] = ITEMS;
 
-    
-    constructor() {
-      this.itemsList = ITEMS;
-      this.radioSelected = "item_3";
-      this.getSelecteditem();
-     }
+          
+          constructor() {
+            this.itemsList = ITEMS;
+            //Selecting Default Radio item here
+            this.radioSelected = "item_3";
+            this.getSelecteditem();
+          }
 
-    getSelecteditem(){
-      this.radioSel = ITEMS.find(Item => Item.value === this.radioSelected);
-      this.radioSelectedString = JSON.stringify(this.radioSel);
-    }
+          // Get row item from array  
+          getSelecteditem(){
+            this.radioSel = ITEMS.find(Item => Item.value === this.radioSelected);
+            this.radioSelectedString = JSON.stringify(this.radioSel);
+          }
+          // Radio Change Event
+          onItemChange(item){
+            this.getSelecteditem();
+          }
 
-    onItemChange(item){
-      this.getSelecteditem();
-    }
-
-}
+      }
--- Demo/Angular/radioButtonExample/src/app/mock-data.ts
+++ Demo/Angular/radioButtonExample/src/app/mock-data.ts
@@ -1,24 +1,24 @@
 import { Item } from './item';
 
-export const ITEMS: Item[] = [
-    {
-        name:'Item 1',
-        value:'item_1'
-     },
-     {
-         name:'Item 2',
-         value:'item_2'
-      },
-      {
-          name:'Item 3',
-          value:'item_3'
-       },
-       {
-           name:'Item 4',
-           value:'item_4'
-        },
-        {
-            name:'Item 5',
-            value:'item_5'
-         }
-];
+            export const ITEMS: Item[] = [
+                {
+                    name:'Item 1',
+                    value:'item_1'
+                },
+                {
+                    name:'Item 2',
+                    value:'item_2'
+                },
+                {
+                    name:'Item 3',
+                    value:'item_3'
+                },
+                {
+                    name:'Item 4',
+                    value:'item_4'
+                    },
+                    {
+                        name:'Item 5',
+                        value:'item_5'
+                    }
+            ];
